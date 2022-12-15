ebdd34d9a0ac9f9627593ed585c3e2bc14eb9357
MauroGith <mauro_rk_@hotmail.com>
2022-12-15 19:33:53 -0300

    update

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -15,6 +15,7 @@ function Project() {
     const { id } = useParams()
     
     const [project, setProject] = useState([])
+    const [services, setServices] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
     const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
@@ -83,8 +84,18 @@ function Project() {
 
         project.cost = newCost
 
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-type' : 'application/json',
+            },
+            body: JSON.stringify(project)
+        })
+            .then(response => response.json())
+            .then(data => setServices(data.services))
+            .catch(error => console.log(error))
 
-        toggleServiceForm(false)
+            toggleServiceForm(false)
     }
     
 
@@ -112,6 +123,7 @@ function Project() {
             .then(response => response.json())
             .then(() => {
                 setProject(projectUpdated)
+                setServices(servicesUpdated)
                 setMessage('Serviço removido com sucesso!')
                 setType('success')
             })
@@ -180,6 +192,9 @@ function Project() {
                         }
                     </div>
 
+                    {project.services.length}
+                    {services.length}
+                    
                     <div className={styles.service_box}>
                         {project.services.length > 0 && 
                             project.services.map(service => (
